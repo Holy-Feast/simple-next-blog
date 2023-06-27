9f682833dc7606bbd502cbc8616ec0ced382bb39
Holy-Feast <shelofasstov@gmail.com>
2023-06-27 13:45:59 +0300

    fixed component home

--- components/pages/home/Home.js
+++ components/pages/home/Home.js
@@ -1,39 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../../../state/actions/actions';
 import PostListComponent from '../../PostsList/PostsList';
 import { Container, Title } from './styles';
 
-const Home = () => {
-    const [posts, setPosts] = useState([]);
+const Home = ({ posts, fetchPosts }) => {
+  useEffect(() => {
+    fetchPosts();
+  }, [fetchPosts]);
 
-    useEffect(() => {
-        fetchPosts();
-    }, []);
-
-    const fetchPosts = async () => {
-        try {
-            const response = await axios.get(
-                'https://jsonplaceholder.typicode.com/posts?_limit=10'
-            );
-            setPosts(response.data);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    return (
-        <Container>
-            <Title>Latest Posts</Title>
-            {posts.length ? (
-                <PostListComponent
-                posts={posts}
-                isButtons={false}
-            />
-            ) : (
-                <div>No posts</div>
-            )}
-        </Container>
-    );
+  return (
+    <Container>
+      <Title>Latest Posts</Title>
+      {posts.length ? (
+        <PostListComponent posts={posts} isButtons={false} />
+      ) : (
+        <div>No posts</div>
+      )}
+    </Container>
+  );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    posts: state.posts,
+  };
+};
+
+const mapDispatchToProps = {
+  fetchPosts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
