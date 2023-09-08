febc56313891da4731f319ab17efd159ee598505
Holy-Feast <shelofasstov@gmail.com>
2023-09-08 14:42:47 +0300

    fix env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  env: {
+    USERS_API_URL: 'https://647cc089c0bae2880ad1233e.mockapi.io/api/blog/users',
+    BLOG_API_URL: 'https://647cc089c0bae2880ad1233e.mockapi.io/api/blog/blog/',
+  },
 };
 
 module.exports = nextConfig;
